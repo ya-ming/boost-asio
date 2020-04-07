df58787c2c240d014d8e070240562ef128d9532b
YaMing Wu <wuyaming@gmail.com>
2020-04-06 19:56:11 -0500

    formatting the code

--- 02-link-boost-lib-example.cpp
+++ 02-link-boost-lib-example.cpp
@@ -4,14 +4,14 @@
 
 int main()
 {
-    std::string line;
-    boost::regex pat( "^Subject: (Re: |Aw: )*(.*)" );
+  std::string line;
+  boost::regex pat("^Subject: (Re: |Aw: )*(.*)");
 
-    while (std::cin)
-    {
-        std::getline(std::cin, line);
-        boost::smatch matches;
-        if (boost::regex_match(line, matches, pat))
-            std::cout << matches[2] << std::endl;
-    }
+  while (std::cin)
+  {
+    std::getline(std::cin, line);
+    boost::smatch matches;
+    if (boost::regex_match(line, matches, pat))
+      std::cout << matches[2] << std::endl;
+  }
 }
